070ae304f9708d60c50a3b28b42977fa2878c0f7
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-11 11:40:57 +0900

    #1 첫 번째 커밋: atom.js 하나로 redux와 같은 기능을 사용할 수 있다.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react";
+import { RecoilRoot,useRecoilState,useRecoilValue } from "recoil";
+import countState from "./atom";
+
+function Count2() {
+  const count = useRecoilValue(countState);
+  return (
+    <p>{count}</p>
+  )
+}
+
+function Count() {
+  const [count, setCount] = useRecoilState(countState);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+    return (
+      <div>
+        <button onClick={handleClick}>증가</button>
+        <p>{count}</p>
+      </div>
+    )
+}
+
+function Main() {
+  return (
+    <>
+    <Count />
+    <Count2 />
+    <Count2 />
+    </>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <Main />
+    </RecoilRoot>
   );
 }
 
